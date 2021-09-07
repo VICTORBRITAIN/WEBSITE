503f747e5266a454c0eb2abf560f55ab46e0adaa
Andrew Davidson <a-davidson1@hotmail.co.uk>
2021-09-07 17:42:36 +0100

    --- Auto-Git Commit ---

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Andrew Davidson/WEBSITE
+# Build directory: C:/Users/Andrew Davidson/WEBSITE/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
